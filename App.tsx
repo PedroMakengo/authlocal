37186e67e59c4a24dc38f3cd6b79a722c6bcbb33
PedroMakengo <pedromakengomiguel@gmail.com>
2024-05-20 16:59:44 +0100

    Adicionado autenticação com o expo-local-authentation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react'
+import { Text, View, Button, Alert } from 'react-native'
+import { styles } from './styles'
+
+import * as LocalAuthentication from 'expo-local-authentication'
 
 export default function App() {
+  const [isAuthenticate, setIsAuthenticate] = useState(false)
+
+  async function verifyAvailableAuthentication() {
+    const compatible = await LocalAuthentication.hasHardwareAsync()
+    console.log(compatible)
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync()
+
+    console.log(
+      types.map((type) => LocalAuthentication.AuthenticationType[type])
+    )
+  }
+
+  async function handleAuthentication() {
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync()
+
+    if (!isBiometricEnrolled) {
+      return Alert.alert(
+        'Login',
+        'Nenhuma biometria encontrada. Por favor, cadastre no dispositivo.'
+      )
+    }
+
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com Biometria',
+      fallbackLabel: 'Biometria não reconhecida',
+    })
+
+    setIsAuthenticate(auth.success)
+  }
+
+  useEffect(() => {
+    verifyAvailableAuthentication()
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Usuário conectado: {isAuthenticate ? 'Sim' : 'Não'} </Text>
+      <Button title="Entrar" onPress={handleAuthentication} />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
